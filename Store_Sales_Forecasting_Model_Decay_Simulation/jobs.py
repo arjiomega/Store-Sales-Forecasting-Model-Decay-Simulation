ea1d26185949d032e303c5bfd64291f4d3dd415a
arjiomega <richardjoseph.omega@gmail.com>
2024-03-22 15:28:46 +0800

    add segmented and reports job

--- Store_Sales_Forecasting_Model_Decay_Simulation/jobs.py
+++ Store_Sales_Forecasting_Model_Decay_Simulation/jobs.py
@@ -1,7 +1,11 @@
 from dagster import define_asset_job
 
-from .assets import core_partitioned_assets
+from .assets import partitioned_assets_, segmented_assets_, reports_assets
 
 partitioned_load_data_job = define_asset_job(
-    "load_data_job", selection=core_partitioned_assets
+    "load_data_job", selection=partitioned_assets_
 )
+
+segmented_data_job = define_asset_job("segmented_data_job", selection=segmented_assets_)
+
+reports_job = define_asset_job("reports_job", selection=reports_assets)
